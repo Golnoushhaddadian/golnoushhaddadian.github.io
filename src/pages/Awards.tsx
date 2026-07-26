d4a85c0880dc5d0c733cb11b9b6b07b6b5324bdd
Lia Haddadian <84669235+Golnoushhaddadian@users.noreply.github.com>
2026-07-26 18:12:11 -0400

    Update Awards page layout width

--- src/pages/Awards.tsx
+++ src/pages/Awards.tsx
@@ -126,7 +126,7 @@ const Awards = () => {
   });
 
   return (
-    <div className="max-w-4xl mx-auto">
+    <div className="max-w-5xl mx-auto">
       <h1 className="text-xl sm:text-2xl md:text-3xl font-bold mb-10 sm:mb-14 text-center">
         Awards &amp; Honors
       </h1>
